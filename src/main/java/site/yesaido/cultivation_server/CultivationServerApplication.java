5b6760281b662fde836081c5535ec3fe02629cea
Poly-Etilen <wlsdud04060@naver.com>
2026-07-15 13:11:01 +0900

    first commit

---
+++ src/main/java/site/yesaido/cultivation_server/CultivationServerApplication.java
@@ -0,0 +1,13 @@
+package site.yesaido.cultivation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultivationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultivationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/yesaido/cultivation_server/CultivationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.yesaido.cultivation_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultivationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
