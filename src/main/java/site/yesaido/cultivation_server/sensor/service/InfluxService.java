408d04fc4dfdb2ab2314dbbb44fe96be90a6c8c9
dyesria <dyeskorea0131@gmail.com>
2026-08-18 10:37:19 +0900

    merged

--- src/main/java/site/yesaido/cultivation_server/sensor/service/InfluxService.java
+++ src/main/java/site/yesaido/cultivation_server/sensor/service/InfluxService.java
@@ -1,5 +1,6 @@
 package site.yesaido.cultivation_server.sensor.service;
 
+import site.yesaido.cultivation_server.rabbitmq.event.SensorValueEvent;
 import site.yesaido.cultivation_server.sensor.dto.response.influx.LatestSensorValueResponse;
 import site.yesaido.cultivation_server.sensor.dto.response.influx.SensorTrendPointListResponse;
 import site.yesaido.cultivation_server.sensor.dto.response.influx.SensorTypeAverageResponse;
@@ -8,10 +9,12 @@ import java.util.List;
 
 public interface InfluxService {
 
-
+    // 센서·단위·장치별 최신값
     List<LatestSensorValueResponse> findLatestByCultivationId(long cultivationId);
 
+    // 최근 24시간 센서 종류, 단위는 통합한 평균
     List<SensorTypeAverageResponse> findAverageByCultivationIdForLast24Hours(long cultivationId);
 
+    // 특정 장치와 센서 종류의 최근 24시간 데이터를 15분 평균으로 반환
     SensorTrendPointListResponse findTrend(long cultivationId, String deviceEui, String sensorType);
 }
