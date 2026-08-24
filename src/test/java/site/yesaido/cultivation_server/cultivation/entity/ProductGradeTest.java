9b05a3b905aae71d6d9b9cba1aea8341cebd2cbd
Poly-Etilen <wlsdud04060@naver.com>
2026-08-24 13:20:15 +0900

    refactor: import문 최적화

--- src/test/java/site/yesaido/cultivation_server/cultivation/entity/ProductGradeTest.java
+++ src/test/java/site/yesaido/cultivation_server/cultivation/entity/ProductGradeTest.java
@@ -1,7 +1,6 @@
 package site.yesaido.cultivation_server.cultivation.entity;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import site.yesaido.cultivation_server.cultivation.entity.harvest.ProductGrade;
